c47561d26952e961375c01d48c23f262fe34a75f
Kevin Frank <frankevi@fh-brandenburg.de>
2017-11-16 20:03:24 +0100

    - Prefab-Ordner angelegt - Material für Spiegel erstellt - daraus Prefab für Spiegel erstellt - Skript "ReflectionRenderer.cs"  erstellt, welches die Reflection Probe korrekt platziert und das Rendern auslöst - ersten Spiegel im Korridor 3 platziert

---
+++ Assets/Scripts/ReflectionRenderer.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ReflectionRenderer : MonoBehaviour {
+
+	private GameObject fpCameraObject;
+	private GameObject plane;
+	private GameObject reflectionProbeObject;
+
+	private Camera fpCamera;
+	private MeshRenderer planeRenderer;
+	private ReflectionProbe reflectionProbe;
+
+	// Use this for initialization
+	void Start () {
+		fpCameraObject = GameObject.Find("Player/FirstPerson/Camera");
+		plane = transform.Find("Plane").gameObject;
+		reflectionProbeObject = transform.Find("ReflectionProbe").gameObject;
+		fpCamera = fpCameraObject.GetComponent<Camera>();
+		planeRenderer = plane.GetComponent<MeshRenderer>();
+		reflectionProbe = reflectionProbeObject.GetComponent<ReflectionProbe>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		// do not refresh if mirror is not visible
+		if(!isVisible()) {
+			return;
+		}
+		// reposition the reflection probe
+		Vector3 planeNormal = plane.transform.up;
+		Vector3 planePosition = plane.transform.position;
+		Vector3 cameraPosition = fpCameraObject.transform.position;
+
+		Plane _plane = new Plane(planeNormal, planePosition);
+		float distance = _plane.GetDistanceToPoint(cameraPosition);
+		Vector3 direction = planeNormal * -1;
+		Vector3 newPosition = cameraPosition + (direction * distance * 2);
+
+		reflectionProbeObject.transform.position = newPosition;
+		// redraw the reflection
+		reflectionProbe.RenderProbe();
+	}
+
+	private bool isVisible() {
+		Plane[] frustumPlanes = GeometryUtility.CalculateFrustumPlanes(fpCamera);
+		bool visible = GeometryUtility.TestPlanesAABB(frustumPlanes, planeRenderer.bounds);
+		Debug.Log("visible: " + visible);
+		return visible;
+	}
+
+	private bool isPointVisible(Vector3 point) {
+		point = fpCamera.WorldToViewportPoint(point);
+		return point.x > 0 && point.x < 1 && point.y > 0 && point.y < 1 && point.z > 0;
+	}
+}
