0c0ad123e92fbf99e89034d4a2adb7ce273d89d7
Kevin Frank <frankevi@fh-brandenburg.de>
2017-12-14 20:02:20 +0100

    CreditsManager erstellt, welcher die Credits anhand der credits.txt im Resource-Ordner befüllt

---
+++ Assets/Scripts/CreditsManager.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CreditsManager : MonoBehaviour {
+
+	void Start () {
+		Text creditsText = transform.Find("Scroll View").Find("Viewport").Find("Content").Find("Text").GetComponent<Text>();
+		string text = PersistenceManager.readTextResource("credits");
+		creditsText.text = text;
+	}
+}
