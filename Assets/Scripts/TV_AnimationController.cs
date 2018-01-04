99762c5fe045e80ead6e7c812242d33a9ecb91df
Kevin Frank <frankevi@fh-brandenburg.de>
2018-01-04 13:42:30 +0100

    - Skript zur Steuerungen der Animationen für die Monitore erstellt und Prefab dazu angepasst - Animationen für die Monitoren angepasst - Texturen für die Anzeige an den Monitoren importiert - die ersten 2 Monitore im Raum 1 platziert und geskripted

---
+++ Assets/Scripts/TV_AnimationController.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TV_AnimationController : MonoBehaviour {
+
+	public float startDelay = 0f;
+	public float noiseDuration = 0f;
+	public bool isMonitor2 = false;
+
+	private TV_AnimationController parentController;
+	private GameObject playerObject;
+	private Animator tvAnimator;
+	private MeshRenderer monitorImageRenderer;
+
+	// Use this for initialization
+	void Start () {
+		// check for type by existing animator component
+		tvAnimator = GetComponent<Animator>();
+		if(tvAnimator != null) {
+			// game object is TV
+			monitorImageRenderer = transform.Find("MonitorImage").GetComponent<MeshRenderer>();
+		} else if(GetComponent<Collider>() != null) {
+			// game object is trigger
+			playerObject = GameObject.Find("Player");
+			parentController = transform.parent.GetComponent<TV_AnimationController>();
+		} else {
+			// game object is none of the above >> remove this script
+			Debug.Log("TV_AnimationController hängt weder an einem TV noch an einem TriggerCollider");
+			Destroy(this);
+		}
+	}
+
+	void OnTriggerEnter(Collider other) {
+		OnTriggerStay(other);
+	}
+
+	void OnTriggerStay(Collider other) {
+		if(isMonitor2) {
+			// check for correct rotation
+			float rotation = playerObject.transform.eulerAngles.y;
+			if(rotation < 250 && rotation > -70) {
+				return;
+			}
+		}
+		parentController.startTvAnimation();
+		Destroy(gameObject);
+	}
+
+	private void startTvAnimation() {
+		StartCoroutine(tvAnimationCoroutine());
+	}
+
+	private IEnumerator tvAnimationCoroutine() {
+		yield return new WaitForSeconds(startDelay);
+		tvAnimator.SetInteger("state", 1);
+		yield return new WaitForSeconds(noiseDuration);
+		tvAnimator.SetInteger("state", 2);
+	}
+}
