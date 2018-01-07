3cd0765772b5f481eb3b94f47a39e397433be0bf
Kevin Frank <frankevi@fh-brandenburg.de>
2018-01-07 11:46:28 +0100

    - Prefab und Skript zum Resetten des Playeres, wenn er in den Abgrund fällt - Resets in der LevelScene hinzugefügt

---
+++ Assets/Scripts/ResetCharacter.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResetCharacter : MonoBehaviour {
+
+	public Vector3 resetToPosition;
+	public float resetToRotation;
+
+	private GameObject player;
+	private FPCameraController cameraController;
+
+	// Use this for initialization
+	void Start () {
+		player = GameObject.Find("/Player");
+		cameraController = player.transform.Find("FirstPerson/Camera").GetComponent<FPCameraController>();
+	}
+
+	void OnTriggerEnter(Collider other) {
+		player.transform.position = resetToPosition;
+		cameraController.setRotation(Quaternion.Euler(new Vector3(0f, resetToRotation, 0f)));
+		// TODO play sound
+	}
+}
